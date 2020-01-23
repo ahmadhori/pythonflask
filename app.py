b5f35944f6b896e956a3d510cdce89d7d944514e
Ahmad Houri <Ahmad.hori@gmail.com>
2020-01-23 19:48:05 +0100

    returned a json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify  # importing Flask constructor from flask package
 app = Flask(__name__)
 
 
@@ -7,5 +7,16 @@ def hello_world():
     return "Hello World"
 
 
+@app.route('/hi')
+def hi():
+    return "You are welcome"
+
+
+@app.route('/json')
+def json():
+    student = dict(firstname="ahmad", lastname="houri")
+    return jsonify(student)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
